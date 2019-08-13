74ac88bf92c08c50055e9a1ab0a3f27674640210
Kristofer Hallin <kristofer@sunet.se>
2019-08-13 13:36:49 +0200

    Functionality for device configuration handling

--- cnaas/frontend/views.py
+++ cnaas/frontend/views.py
@@ -1,50 +1,64 @@
 from django.shortcuts import render
 from cnaas import settings
 
+import time
 import requests
 
 
-#
-# Get all devices
-#
 def devices_get():
-    devices = requests.get(settings.CNAAS_HOST + '/device')
-    return devices.json()['data']['devices']
+    """
+    Get all devices and return as a dict.
+    """
+
+    try:
+        res = requests.get(settings.CNAAS_HOST + '/device')
+        devices = res.json()['data']['devices']
+    except Exception:
+        devices = {}
+
+    return devices
 
 
-#
-# Remove one or more devices
-#
 def device_remove(devices):
+    """
+    Remove one or more devices
+    """
+
+    retval = 0
+
     for _ in devices:
         hostname, device_id = _.split(',')
-        res = requests.delete(settings.CNAAS_HOST + '/device/%s' % device_id)
+        try:
+            res = requests.delete(settings.CNAAS_HOST + '/device/%s' % device_id)
+        except Exception:
+            retval = -1
         if res.status_code != 200:
-            return 'Error'
-    return 'Devices removed successfully'
+            retval = -1
+    return retval
 
 
-#
-# Sync one or more devices
-#
-def device_sync(devices=[], all_devices=False):
-    job_ids = []
+def device_sync(devices=[], all_devices=False, dry_run=True):
+    """
+    Sync one or more devices
+    """
+
     for _ in devices:
         hostname, device_id = _.split(',')
-        jsondict = {'hostname': hostname, 'dry_run': True}
+        jsondict = {'hostname': hostname, 'dry_run': dry_run}
         res = requests.post(settings.CNAAS_HOST + '/device_syncto',
                             json=jsondict,
                             verify=False)
         if res.status_code != 200:
             return 'Error: Failed to get job IDs'
-        job_ids.append(res.json()['job_id'])
-    return job_ids
+        job_id = res.json()['job_id']
+    return job_id
 
 
-#
-# Refresh templates
-#
 def templates_refresh():
+    """
+    Refresh templates
+    """
+
     jsondict = {'action': 'refresh'}
     res = requests.put(settings.CNAAS_HOST + '/repository/templates',
                        json=jsondict)
@@ -53,10 +67,11 @@ def templates_refresh():
     return 'Update succeded: \\n' + res.json()['data'].rstrip()
 
 
-#
-# Refresh settings
-#
 def settings_refresh():
+    """
+    Refresh settings
+    """
+
     jsondict = {'action': 'refresh'}
     res = requests.put(settings.CNAAS_HOST + '/repository/settings',
                        json=jsondict)
@@ -65,28 +80,43 @@ def settings_refresh():
     return 'Update succeded: \\n' + res.json()['data'].rstrip()
 
 
-#
-# Get jobs
-#
-def jobs_get():
-    jobs = requests.get(settings.CNAAS_HOST + '/job', verify=False)
-    return jobs.json()
+def job_get(job_id=None):
+    """
+    Get jobs
+    """
+
+    job_url = settings.CNAAS_HOST + '/job'
+    if job_id is not None:
+        job_url += '/' + str(job_id)
+    res = requests.get(job_url, verify=False)
+    if res.status_code != 200:
+        return {}
+    return res.json()['data']['jobs']
 
 
-#
-# Index view
-#
+def job_status(job_id):
+    """
+    Get job status
+    """
+
+    res = job_get(job_id=job_id)
+    return res[0]['status']
+
+
 def index(request):
+    """
+    Index page
+    """
+
     return render(request, 'index.html', {})
 
 
-#
-# Device view
-#
 def devices(request):
-    data = {}
-    sync_ids = {}
+    """
+    Devices page
+    """
 
+    data = {}
     data['devices'] = devices_get()
 
     # If we don't get a POST, just render the page
@@ -98,12 +128,14 @@ def devices(request):
     if 'remove' in request.POST:
         res = device_remove(devices_selected)
         data['message'] = res
+    elif 'sync_dryrun' in request.POST:
+        data['dryrun'] = True
+        return sync(request, job_id=device_sync(devices_selected), dryrun=True)
     elif 'sync' in request.POST:
-        sync_ids = device_sync(devices_selected)
-        data['sync_ids'] = sync_ids
-    elif 'sync_all' in request.POST:
-        sync_ids = device_sync(all_devices=True)
-        data['sync_ids'] = sync_ids
+        data['dryrun'] = False
+        return sync(request,
+                    job_id=device_sync(devices_selected, dry_run=False),
+                    dryrun=False)
     elif 'refresh_templates' in request.POST:
         res = templates_refresh()
         data['message'] = res
@@ -114,17 +146,30 @@ def devices(request):
     return render(request, 'devices.html', context=data)
 
 
-#
-# Jobs view
-#
-def jobs(request):
-    jobs = jobs_get()['data']['jobs']
-    return render(request, 'jobs.html', {'jobs': jobs})
+def sync(request, job_id=None, devices=None, dryrun=True):
+    """
+    Sync page
+    """
 
+    data = {}
+    for i in range(60):
+        time.sleep(2)
+        if job_status(job_id) == 'FINISHED':
+            break
+    else:
+        data['message'] = 'Device synchronisation failed'
+    res = job_get(job_id)[0]
+    devices = res['result'].keys()
+    data['dryrun'] = dryrun
+    data['devices'] = devices
+    data['device_data'] = list()
 
-#
-# Sync view
-#
-def sync(request):
-    print(request)
-    return render(request, 'sync.html', {})
+    for device in devices:
+        for result in res['result'][device]:
+            if type(result) is not dict:
+                continue
+            if result['name'] == 'push_sync_device':
+                continue
+            result['hostname'] = device
+            data['device_data'].append(result)
+    return render(request, 'sync.html', context=data)
