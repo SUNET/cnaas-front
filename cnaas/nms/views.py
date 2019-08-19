2b65299ea3ca040a1cdb28cbcd32008cd9e9de03
Kristofer Hallin <kristofer@sunet.se>
2019-08-19 15:57:45 +0200

    * Add support for adding/removing management domains * Fixed inentatation * More comments

--- cnaas/nms/views.py
+++ cnaas/nms/views.py
@@ -6,241 +6,279 @@ import requests
 
 
 def devices_get():
-    """
-    Get all devices and return as a dict.
-    """
+	"""
+	Get all devices and return as a dict.
+	"""
 
-    try:
-        res = requests.get(settings.CNAAS_HOST + '/device', verify=False)
-        devices = res.json()['data']['devices']
-    except Exception:
-        devices = {}
+	try:
+		res = requests.get(settings.CNAAS_HOST + '/device', verify=False)
+		devices = res.json()['data']['devices']
+	except Exception:
+		devices = {}
 
-    return devices
+	return devices
 
 
 def device_remove(devices):
-    """
-    Remove one or more devices
-    """
+	"""
+	Remove one or more devices
+	"""
 
-    retval = 0
+	retval = 0
 
-    for _ in devices:
-        hostname, device_id = _.split(',')
-        try:
-            res = requests.delete(settings.CNAAS_HOST + '/device/%s' % device_id,
-                                  verify=False)
-        except Exception:
-            retval = -1
-        if res.status_code != 200:
-            retval = -1
-    return retval
+	for _ in devices:
+		hostname, device_id = _.split(',')
+		try:
+			res = requests.delete(settings.CNAAS_HOST + '/device/%s' % device_id,
+								  verify=False)
+		except Exception:
+			retval = -1
+		if res.status_code != 200:
+			retval = -1
+	return retval
 
 
 def device_sync(devices=[], all_devices=False, dry_run=True):
-    """
-    Sync one or more devices
-    """
+	"""
+	Sync one or more devices
+	"""
 
-    for _ in devices:
-        hostname, device_id = _.split(',')
-        jsondict = {'hostname': hostname, 'dry_run': dry_run}
-        res = requests.post(settings.CNAAS_HOST + '/device_syncto',
-                            json=jsondict,
-                            verify=False)
-        if res.status_code != 200:
-            return 'Error: Failed to get job IDs'
-        job_id = res.json()['job_id']
-    return job_id
+	for _ in devices:
+		hostname, device_id = _.split(',')
+		jsondict = {'hostname': hostname, 'dry_run': dry_run}
+		res = requests.post(settings.CNAAS_HOST + '/device_syncto',
+							json=jsondict,
+							verify=False)
+		if res.status_code != 200:
+			return 'Error: Failed to get job IDs'
+		job_id = res.json()['job_id']
+	return job_id
 
 
 def device_interfaces(hostname):
-    """
-    Get device interfaces
-    """
+	"""
+	Get device interfaces
+	"""
 
-    try:
-        res = requests.get(settings.CNAAS_HOST + '/device/' + hostname + '/interfaces',
-                           verify=False)
-    except Exception:
-        return None
-    interfaces = res.json()['data']['interfaces']
-    return interfaces
+	try:
+		res = requests.get(settings.CNAAS_HOST + '/device/' + hostname + '/interfaces',
+						   verify=False)
+	except Exception:
+		return None
+	interfaces = res.json()['data']['interfaces']
+	return interfaces
 
 
 def templates_refresh():
-    """
-    Refresh templates
-    """
+	"""
+	Refresh templates
+	"""
 
-    jsondict = {'action': 'refresh'}
-    res = requests.put(settings.CNAAS_HOST + '/repository/templates',
-                       json=jsondict,
-                       verify=False)
-    if res.status_code != 200:
-        return 'Error: \\nFailed to refresh templates'
-    return 'Update succeded: \\n' + res.json()['data'].rstrip()
+	jsondict = {'action': 'refresh'}
+	res = requests.put(settings.CNAAS_HOST + '/repository/templates',
+					   json=jsondict,
+					   verify=False)
+	if res.status_code != 200:
+		return 'Error: \\nFailed to refresh templates'
+	return 'Update succeded: \\n' + res.json()['data'].rstrip()
 
 
 def settings_refresh():
-    """
-    Refresh settings
-    """
+	"""
+	Refresh settings
+	"""
 
-    jsondict = {'action': 'refresh'}
-    res = requests.put(settings.CNAAS_HOST + '/repository/settings',
-                       json=jsondict,
-                       verify=False)
-    if res.status_code != 200:
-        return 'Error: \\nFailed to refresh settings'
-    return 'Update succeded: \\n' + res.json()['data'].rstrip()
+	jsondict = {'action': 'refresh'}
+	res = requests.put(settings.CNAAS_HOST + '/repository/settings',
+					   json=jsondict,
+					   verify=False)
+	if res.status_code != 200:
+		return 'Error: \\nFailed to refresh settings'
+	return 'Update succeded: \\n' + res.json()['data'].rstrip()
 
 
 def job_get(job_id=None):
-    """
-    Get jobs
-    """
+	"""
+	Get jobs
+	"""
 
-    job_url = settings.CNAAS_HOST + '/job'
-    if job_id is not None:
-        job_url += '/' + str(job_id)
-    res = requests.get(job_url, verify=False)
-    if res.status_code != 200:
-        return {}
-    return res.json()['data']['jobs']
+	job_url = settings.CNAAS_HOST + '/job'
+	if job_id is not None:
+		job_url += '/' + str(job_id)
+	res = requests.get(job_url, verify=False)
+	if res.status_code != 200:
+		return {}
+	return res.json()['data']['jobs']
 
 
 def job_status(job_id):
-    """
-    Get job status
-    """
+	"""
+	Get job status
+	"""
 
-    res = job_get(job_id=job_id)
-    return res[0]['status']
+	res = job_get(job_id=job_id)
+	return res[0]['status']
 
 
 def mgmtdomains_get():
-    """
-    Get all management domains and return as a dict.
-    """
+	"""
+	Get all management domains and return as a dict.
+	"""
 
-    try:
-        res = requests.get(settings.CNAAS_HOST + '/mgmtdomain', verify=False)
-        domains = res.json()['data']['mgmtdomains']
-    except Exception:
-        domains = {}
+	try:
+		res = requests.get(settings.CNAAS_HOST + 'mgmtdomain', verify=False)
+		domains = res.json()['data']['mgmtdomains']
+	except Exception:
+		domains = {}
 
-    return domains
+	return domains
 
 
 def index(request):
-    """
-    Index page
-    """
+	"""
+	Index page
+	"""
 
-    return render(request, 'index.html', {})
+	return render(request, 'index.html', {})
 
 
 def devices(request):
-    """
-    Devices page
-    """
+	"""
+	Devices page
+	"""
 
-    data = {}
-    data['devices'] = devices_get()
-    for _ in data['devices']:
-        interfaces = device_interfaces(_['hostname'])
-        if interfaces is []:
-            continue
-        _['interfaces'] = interfaces
+	data = {}
+	data['devices'] = devices_get()
+	for _ in data['devices']:
+		interfaces = device_interfaces(_['hostname'])
+		if interfaces is []:
+			continue
+		_['interfaces'] = interfaces
 
-    # If we don't get a POST, just render the page
-    if not request.POST:
-        return render(request, 'devices.html', data)
+	# If we don't get a POST, just render the page
+	if not request.POST:
+		return render(request, 'devices.html', data)
 
-    devices_selected = request.POST.getlist('device')
+	devices_selected = request.POST.getlist('device')
 
-    if 'remove' in request.POST:
-        res = device_remove(devices_selected)
-        data['message'] = res
-    elif 'sync_dryrun' in request.POST:
-        if devices_selected == []:
-            return render(request, 'devices.html', context=data)
-        data['dryrun'] = True
-        return sync(request, job_id=device_sync(devices_selected), dryrun=True)
-    elif 'sync' in request.POST:
-        if devices_selected == []:
-            return render(request, 'devices.html', context=data)
-        data['dryrun'] = False
-        return sync(request,
-                    job_id=device_sync(devices_selected, dry_run=False),
-                    dryrun=False)
-    elif 'refresh_templates' in request.POST:
-        res = templates_refresh()
-        data['message'] = res
-    elif 'refresh_settings' in request.POST:
-        res = settings_refresh()
-        data['message'] = res
+	if 'remove' in request.POST:
+		res = device_remove(devices_selected)
+		data['message'] = res
+	elif 'sync_dryrun' in request.POST:
+		if devices_selected == []:
+			return render(request, 'devices.html', context=data)
+		data['dryrun'] = True
+		return sync(request, job_id=device_sync(devices_selected), dryrun=True)
+	elif 'sync' in request.POST:
+		if devices_selected == []:
+			return render(request, 'devices.html', context=data)
+		data['dryrun'] = False
+		return sync(request,
+					job_id=device_sync(devices_selected, dry_run=False),
+					dryrun=False)
+	elif 'refresh_templates' in request.POST:
+		res = templates_refresh()
+		data['message'] = res
+	elif 'refresh_settings' in request.POST:
+		res = settings_refresh()
+		data['message'] = res
 
-    return render(request, 'devices.html', context=data)
+	return render(request, 'devices.html', context=data)
 
 
 def sync(request, job_id=None, devices=None, dryrun=True):
-    """
-    Sync page
-    """
+	"""
+	Sync page
+	"""
 
-    data = {}
-    for i in range(60):
-        time.sleep(2)
-        if job_status(job_id) == 'FINISHED':
-            break
-    else:
-        data['message'] = 'Device synchronisation failed'
-    res = job_get(job_id)[0]
-    devices = res['result'].keys()
-    data['dryrun'] = dryrun
-    data['devices'] = devices
-    data['device_data'] = list()
+	data = {}
+	for i in range(60):
+		time.sleep(2)
+		if job_status(job_id) == 'FINISHED':
+			break
+	else:
+		data['message'] = 'Device synchronisation failed'
+	res = job_get(job_id)[0]
+	devices = res['result'].keys()
+	data['dryrun'] = dryrun
+	data['devices'] = devices
+	data['device_data'] = list()
 
-    for device in devices:
-        for result in res['result'][device]:
-            if type(result) is not dict:
-                continue
-            if result['name'] == 'push_sync_device':
-                continue
-            result['hostname'] = device
-            data['device_data'].append(result)
-    return render(request, 'sync.html', context=data)
+	for device in devices:
+		for result in res['result'][device]:
+			if type(result) is not dict:
+				continue
+			if result['name'] == 'push_sync_device':
+				continue
+			result['hostname'] = device
+			data['device_data'].append(result)
+	return render(request, 'sync.html', context=data)
 
 
 def jobs(request):
-    data = {}
-    data['jobs'] = job_get()
+	data = {}
+	data['jobs'] = job_get()
 
-    return render(request, 'jobs.html', context=data)
+	return render(request, 'jobs.html', context=data)
+
+def mgmtdomain_add(mgmtdict):
+	res = requests.post(settings.CNAAS_HOST + 'mgmtdomain',
+						json=mgmtdict,
+						verify=False)
+	print(res.status_code)
+	if res.status_code != 200:
+		return -1
+	return 0
+
+
+def mgmtdomain_remove(domains):
+	retval = 0
+
+	for domain in domains:
+		try:
+			res = requests.delete(settings.CNAAS_HOST + 'mgmtdomain/%s' % domain,
+								  verify=False)
+		except Exception as e:
+			retval = -1
+		if res.status_code != 200:
+			retval = -1
+	return retval
 
 
 def mgmtdomains(request):
-    data = {}
+	data = {}
 
-    if request.POST:
-        mgmtdomain = {}
-        mgmtdomain['ipv4_gw'] = None
-        mgmtdomain['device_a_id'] = None
-        mgmtdomain['device_a_ip'] = None
-        mgmtdomain['device_b_id'] = None
-        mgmtdomain['device_b_ip'] = None
-        mgmtdomain['vlan'] = None
-        mgmtdomain['description'] = None
+	if not request.POST:
+		data['mgmtdomains'] = mgmtdomains_get()
+		print(data)
+		return render(request, 'mgmtdomains.html', context=data)
 
-    data['mgmtdomains'] = mgmtdomains_get()
+	if 'domain_add' in request.POST:
+		mgmtdomain = {}
+		if 'ipv4_gw' in request.POST:
+			mgmtdomain['ipv4_gw'] = request.POST['ipv4_gw']
+		if 'device_a_id' in request.POST:
+			mgmtdomain['device_a_id'] = int(request.POST['device_a_id'])
+		if 'device_a_ip' in request.POST:
+			mgmtdomain['device_a_ip'] = request.POST['device_a_ip']
+		if 'device_b_id' in request.POST:
+			mgmtdomain['device_b_id'] = int(request.POST['device_b_id'])
+		if 'device_b_ip' in request.POST:
+			mgmtdomain['device_b_ip'] = request.POST['device_b_ip']
+		if 'vlan' in request.POST:
+			mgmtdomain['vlan'] = int(request.POST['vlan'])
+		if 'description' in request.POST:
+			mgmtdomain['description'] = request.POST['description']
+		mgmtdomain_add(mgmtdomain)
+	elif 'domain_delete' in request.POST:
+		mgmtdomain_remove(request.POST.getlist('selected'))
 
-    return render(request, 'mgmtdomains.html', context=data)
+	data['mgmtdomains'] = mgmtdomains_get()
+
+	return render(request, 'mgmtdomains.html', context=data)
 
 
 def linknets(request):
-    data = {}
+	data = {}
 
-    return render(request, 'linknets.html', context=data)
+	return render(request, 'linknets.html', context=data)
