20fb9f3661d7cb1f39aa764db56eb2938e38c81a
Nathalie Jonsson <njons@sunet.se>
2019-11-15 13:55:21 +0100

    adds unordered list to Panel to render API data

--- react-front/public/components/Panel.js
+++ react-front/public/components/Panel.js
@@ -3,6 +3,21 @@ import React from "react";
 class Panel extends React.Component {
   render() {
     console.log("this is device data (in panel)", this.props.responseData);
+    let deviceInfo = "";
+    const responseData = this.props.responseData;
+    deviceInfo = responseData.map((items, index) => {
+      return (
+        <ul key={index}>
+          <li key="0">{items.id}</li>
+          <li key="1"> {items.hostname}</li>
+          <li key="2">{items.description}</li>
+          <li key="3">{items.device_type}</li>
+          <li key="4">{items.last_seen}</li>
+          <li key="5">{items.management_ip}</li>
+        </ul>
+      );
+    });
+
     return (
       <div id="panel">
         <h1> This is the panel</h1>
@@ -13,9 +28,7 @@ class Panel extends React.Component {
         </div>
         <div id="response">
           <h2> Get the response</h2>
-          <div id="data">
-          {/* data goes here */}
-          </div>
+          <div id="data">{deviceInfo}</div>
         </div>
       </div>
     );
