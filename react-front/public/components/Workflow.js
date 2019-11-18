30d6135749ab6707e42b483b052812790524cb83
Nathalie Jonsson <njons@sunet.se>
2019-11-18 14:57:49 +0100

    adds basic structure to workflow page

--- react-front/public/components/Workflow.js
+++ react-front/public/components/Workflow.js
@@ -7,21 +7,24 @@ class Workflow extends React.Component {
     return (
       <section>
         <h1>Commit changes workflow</h1>
-        <div className="workflow-container__primary">
+        <div className="workflow-container">
           <div className="workflow-container__header">
             <h2>Refresh repositories (1/4)</h2>
-            <button className="workflow-container__button">
-              <a className="workflow-container__button-link" href="#">
-                Close
-              </a>
-            </button>
+            <a href="#">
+              <button className="workflow-container__button">Close</button>
+            </a>
           </div>
           <div className="workflow-container__secondary">
             <p>this is box1 and it will hold 2 buttons</p>
           </div>
         </div>
-        <div className="workflow-container__primary">
-          <h2>Dry run (2/4)</h2>
+        <div className="workflow-container">
+          <div className="workflow-container__header">
+            <h2>Dry run (2/4)</h2>
+            <a href="#">
+              <button className="workflow-container__button">Close</button>
+            </a>
+          </div>
           <div className="workflow-container__secondary">
             <p>
               this is box2 and it will hold text, button to get progressbar
@@ -29,16 +32,26 @@ class Workflow extends React.Component {
             </p>
           </div>
         </div>
-        <div className="workflow-container__primary">
-          <h2>Verify difference (3/4)</h2>
+        <div className="workflow-container">
+          <div className="workflow-container__header">
+            <h2>Verify difference (3/4)</h2>
+            <a href="#">
+              <button className="workflow-container__button">Close</button>
+            </a>
+          </div>
           <div className="workflow-container__secondary">
-            <p>this is box3 this will display results form</p>
+            <p>this is box3 this will display results from step 2 and 2 buttons</p>
           </div>
         </div>
-        <div className="workflow-container__primary">
-          <h2>Commit configuration (4/4)</h2>
+        <div className="workflow-container">
+          <div className="workflow-container__header">
+            <h2>Commit configuration (4/4)</h2>
+            <a href="#">
+              <button className="workflow-container__button">Close</button>
+            </a>
+          </div>
           <div className="workflow-container__secondary">
-            <p>this is box4</p>
+            <p>this is box4 </p>
           </div>
         </div>
       </section>
