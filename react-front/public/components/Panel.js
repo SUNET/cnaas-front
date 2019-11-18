3a9cbb696b15b6a99e8f2e54cfaae347816f32b5
Nathalie Jonsson <njons@sunet.se>
2019-11-18 14:33:19 +0100

    passes getDevicesData from App.js, via Panel.js to DeviceList.js for request to be madeand for data to come through

--- react-front/public/components/Panel.js
+++ react-front/public/components/Panel.js
@@ -4,11 +4,15 @@ import Workflow from "./Workflow";
 
 class Panel extends React.Component {
   render() {
+    console.log("this is props (in panel)", this.props);
     console.log("this is device data (in panel)", this.props.devicesData);
     return (
       <div id="component">
         <h1> This is the panel</h1>
-        <DeviceList devicesData={this.props.devicesData} />
+        <DeviceList
+          getDevicesData={this.props.getDevicesData}
+          devicesData={this.props.devicesData}
+        />
         <Workflow />
       </div>
     );
