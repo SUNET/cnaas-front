266070a4c6bc8705c51b96bb84953091c559ba33
Nathalie Jonsson <njons@sunet.se>
2019-11-18 11:26:27 +0100

    splits out styles for panel and component

--- react-front/public/components/Panel.js
+++ react-front/public/components/Panel.js
@@ -6,7 +6,7 @@ class Panel extends React.Component {
   render() {
     console.log("this is device data (in panel)", this.props.devicesData);
     return (
-      <div id="component">
+      <div id="panel">
         <h1> This is the panel</h1>
         {/* <DeviceList devicesData={this.props.devicesData} /> */}
         <Workflow />
