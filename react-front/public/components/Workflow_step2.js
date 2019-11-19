8d8056e679dcc5ce23b08bed5348f9dc7a540840
Nathalie Jonsson <njons@sunet.se>
2019-11-19 09:08:11 +0100

    adds inital request button in workflow step 2

--- react-front/public/components/Workflow_step2.js
+++ react-front/public/components/Workflow_step2.js
@@ -3,12 +3,12 @@ import React from "react";
 class Workflow_step1 extends React.Component {
   state = {
     // token: "",
-    // commitInfo: [],
-    // latestCommitInfo: []
+    deviceSync: [],
+    deviceSyncStatus: [],
+    deviceSyncJobId: []
     // errorMessage: ""
   };
 
-  // here we check the status of the response and only process the result if status is 200
   checkStatus = response => {
     console.log("we have response");
     console.log("response status:", response.status);
@@ -26,68 +26,42 @@ class Workflow_step1 extends React.Component {
     }
   };
 
-  // getCommitInfo = () => {
-  //   const credentials =
-  //     "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpYXQiOjE1NzEwNTk2MTgsIm5iZiI6MTU3MTA1OTYxOCwianRpIjoiNTQ2MDk2YTUtZTNmOS00NzFlLWE2NTctZWFlYTZkNzA4NmVhIiwic3ViIjoiYWRtaW4iLCJmcmVzaCI6ZmFsc2UsInR5cGUiOiJhY2Nlc3MifQ.Sfffg9oZg_Kmoq7Oe8IoTcbuagpP6nuUXOQzqJpgDfqDq_GM_4zGzt7XxByD4G0q8g4gZGHQnV14TpDer2hJXw";
-  //   console.log("you clicked the get commit info button");
-  //   fetch("https://tug-lab.cnaas.sunet.se:8443/api/v1.0/repository/settings", {
-  //     method: "GET",
-  //     headers: {
-  //       Authorization: `Bearer ${credentials}`
-  //     }
-  //   })
-  //     .then(response => this.checkStatus(response))
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       console.log("this should be data", data);
-  //       {
-  //         this.setState(
-  //           {
-  //             commitInfo: data.data
-  //           },
-  //           () => {
-  //             console.log("this is new state", this.state.commitInfo);
-  //           }
-  //         );
-  //       }
-  //     });
-  // };
-
-  // refreshCommitInfo = () => {
-  //   const credentials =
-  //     "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpYXQiOjE1NzEwNTk2MTgsIm5iZiI6MTU3MTA1OTYxOCwianRpIjoiNTQ2MDk2YTUtZTNmOS00NzFlLWE2NTctZWFlYTZkNzA4NmVhIiwic3ViIjoiYWRtaW4iLCJmcmVzaCI6ZmFsc2UsInR5cGUiOiJhY2Nlc3MifQ.Sfffg9oZg_Kmoq7Oe8IoTcbuagpP6nuUXOQzqJpgDfqDq_GM_4zGzt7XxByD4G0q8g4gZGHQnV14TpDer2hJXw";
-  //   console.log("you clicked the refresh commit info button");
-  //   fetch("https://tug-lab.cnaas.sunet.se:8443/api/v1.0/repository/settings", {
-  //     method: "PUT",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       Authorization: `Bearer ${credentials}`
-  //     },
-  //     body: JSON.stringify({ action: "REFRESH" })
-  //   })
-  //     .then(response => this.checkStatus(response))
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       console.log("this should be data", data);
-  //       {
-  //         this.setState(
-  //           {
-  //             latestCommitInfo: data.data
-  //           },
-  //           () => {
-  //             console.log("this is new state", this.state.latestCommitInfo);
-  //           }
-  //         );
-  //       }
-  //     });
-  // };
+  deviceSyncTo = () => {
+    const credentials =
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpYXQiOjE1NzEwNTk2MTgsIm5iZiI6MTU3MTA1OTYxOCwianRpIjoiNTQ2MDk2YTUtZTNmOS00NzFlLWE2NTctZWFlYTZkNzA4NmVhIiwic3ViIjoiYWRtaW4iLCJmcmVzaCI6ZmFsc2UsInR5cGUiOiJhY2Nlc3MifQ.Sfffg9oZg_Kmoq7Oe8IoTcbuagpP6nuUXOQzqJpgDfqDq_GM_4zGzt7XxByD4G0q8g4gZGHQnV14TpDer2hJXw";
+    console.log("you clicked the start sync info button");
+    fetch("https://tug-lab.cnaas.sunet.se:8443/api/v1.0/device_syncto", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${credentials}`
+      },
+      body: JSON.stringify({ dry_run: true, all: true })
+    })
+      .then(response => this.checkStatus(response))
+      .then(response => response.json())
+      .then(data => {
+        console.log("this should be data", data);
+        {
+          this.setState(
+            {
+              deviceSync: data.data,
+              deviceSyncStatus: data.status,
+              deviceSyncJobId: data.job_id
+            },
+            () => {
+              console.log("this is new state", this.state.deviceSync);
+            }
+          );
+        }
+      });
+  };
 
   render() {
-    // console.log("hello! this is the workflow component");
     // console.log("these are props (in Workflow)", this.props);
-    // console.log("this is repo data (in Workflow)", this.state.commitInfo);
-    // let commitInfo = this.state.commitInfo;
-    // let refreshedCommitInfo = this.state.latestCommitInfo;
+    let syncMessage = this.state.deviceSync;
+    let syncStatus = this.state.deviceSyncStatus;
+    let syncJobId = this.state.deviceSyncJobId;
     return (
       <div className="workflow-container">
         <div className="workflow-container__header">
@@ -97,10 +71,12 @@ class Workflow_step1 extends React.Component {
           </a>
         </div>
         <div className="workflow-collapsable">
-          <p>
-            this is box2 and it will hold text, button to get progressbar
-            data, display final response, three buttons
-            </p>
+          <div className="workflow-collapsable__button-result">
+            <button onClick={this.deviceSyncTo}> Start sync </button>
+            <p>{syncMessage}</p>
+            <p>{syncStatus}</p>
+            <p>{syncJobId}</p>
+          </div>
         </div>
       </div>
     );
