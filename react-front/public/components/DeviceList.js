df02fb7dbb0ddf39d3d3f03efc2d6d95e381b540
Johan Marcusson <johan.marcusson@sunet.se>
2019-11-19 15:08:12 +0100

    Don't need proptype definitions for these parameters since they moved into DeviceList class

--- react-front/public/components/DeviceList.js
+++ react-front/public/components/DeviceList.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from 'prop-types'
 import DeviceSearchForm from "./DeviceSearchForm";
 
 class DeviceList extends React.Component {
@@ -148,11 +147,6 @@ class DeviceList extends React.Component {
       </section>
     );
   }
-}
-
-DeviceList.propTypes = {
-  getDevicesData: PropTypes.func.isRequired,
-  devicesData: PropTypes.array.isRequired
 }
 
 export default DeviceList;
