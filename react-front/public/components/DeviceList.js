74e6c712fad0de2a9a23ee43b5109cae63d23a7e
Nathalie Jonsson <njons@sunet.se>
2019-11-19 12:01:41 +0100

    moves API call from App.js to DeviceList.js - functionality and data no longer passed by props and are now read from state

--- react-front/public/components/DeviceList.js
+++ react-front/public/components/DeviceList.js
@@ -1,12 +1,63 @@
 import React from "react";
 
 class DeviceList extends React.Component {
+  // here we set the initial state of the component
+  state = {
+    // token: "",
+    devicesData: []
+    // errorMessage: ""
+  };
+
+  // here we check the status of the response and only process the result if status is 200
+  checkStatus = response => {
+    console.log("we have response");
+    if (response.status === 200) {
+      console.log("response 200");
+      return Promise.resolve(response);
+    } else if (response.status === 400 || response.status === 401) {
+      this.setState({
+        errorMessage: "Your details were not recognised. Try again!"
+      });
+    } else if (response.staus === 500) {
+      this.setState({
+        errorMessage: "Something went wrong on our end. Try again later."
+      });
+    }
+  };
+
+  getDevicesData = () => {
+    // check that button click works
+    const credentials =
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpYXQiOjE1NzEwNTk2MTgsIm5iZiI6MTU3MTA1OTYxOCwianRpIjoiNTQ2MDk2YTUtZTNmOS00NzFlLWE2NTctZWFlYTZkNzA4NmVhIiwic3ViIjoiYWRtaW4iLCJmcmVzaCI6ZmFsc2UsInR5cGUiOiJhY2Nlc3MifQ.Sfffg9oZg_Kmoq7Oe8IoTcbuagpP6nuUXOQzqJpgDfqDq_GM_4zGzt7XxByD4G0q8g4gZGHQnV14TpDer2hJXw";
+    console.log("you clicked the button");
+    fetch("https://tug-lab.cnaas.sunet.se:8443/api/v1.0/devices", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${credentials}`
+      }
+    })
+      .then(response => this.checkStatus(response))
+      .then(response => response.json())
+      .then(data => {
+        console.log("this should be data", data);
+        {
+          this.setState(
+            {
+              devicesData: data.data.devices
+            },
+            () => {
+              console.log("this is new state", this.state.devicesData);
+            }
+          );
+        }
+      });
+  };
+
   render() {
     console.log("these are props (in DeviceList)", this.props);
-    console.log("this is device data (in DeviceList)", this.props.devicesData);
     let deviceInfo = "";
     // put the response data in a variable
-    const devicesData = this.props.devicesData;
+    const devicesData = this.state.devicesData;
     // .map() through the array of response data
     deviceInfo = devicesData.map((items, index) => {
       // deal with renedering domething for the synchronised boolean
@@ -31,7 +82,7 @@ class DeviceList extends React.Component {
       <section>
         <div id="request">
           {/* <h2> Make a request </h2> */}
-          <button onClick={this.props.getDevicesData}> API request </button>
+          <button onClick={this.getDevicesData}> API request </button>
         </div>
         <div id="response">
           <h2> Get the response</h2>
