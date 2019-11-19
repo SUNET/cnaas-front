2b4262dd03913386cead7aae4a6e9237131a4878
Nathalie Jonsson <njons@sunet.se>
2019-11-19 11:52:57 +0100

    adds in Route from react-router-dom to control what components are displayed at the different routes (DeviceList on home and Workflow on /workflow)

--- react-front/public/components/Panel.js
+++ react-front/public/components/Panel.js
@@ -1,16 +1,15 @@
 import React from "react";
 import DeviceList from "./DeviceList";
 import Workflow from "./Workflow";
+import { Route } from "react-router-dom";
 
 class Panel extends React.Component {
   render() {
     console.log("this is props (in panel)", this.props);
-    console.log("this is device data (in panel)", this.props.devicesData);
     return (
       <div id="panel">
-        {/* <h1> This is the panel</h1> */}
-        {/* <DeviceList devicesData={this.props.devicesData} /> */}
-        <Workflow />
+        <Route exact path="/" render={props => <DeviceList {...props} />} />
+        <Route exact path="/workflow" component={Workflow} />
       </div>
     );
   }
