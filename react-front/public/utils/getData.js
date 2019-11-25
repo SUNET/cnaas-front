adf8554e58e6f58a4a8d70b296d8c5b962e293ac
Nathalie Jonsson <njons@sunet.se>
2019-11-25 21:36:19 +0100

    adds file with separate get request

--- react-front/public/utils/getData.js
+++ react-front/public/utils/getData.js
@@ -1,30 +1,16 @@
-const checkResponseStatus = require("./checkResponseStatus");
-const credentials =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpYXQiOjE1NzEwNTk2MTgsIm5iZiI6MTU3MTA1OTYxOCwianRpIjoiNTQ2MDk2YTUtZTNmOS00NzFlLWE2NTctZWFlYTZkNzA4NmVhIiwic3ViIjoiYWRtaW4iLCJmcmVzaCI6ZmFsc2UsInR5cGUiOiJhY2Nlc3MifQ.Sfffg9oZg_Kmoq7Oe8IoTcbuagpP6nuUXOQzqJpgDfqDq_GM_4zGzt7XxByD4G0q8g4gZGHQnV14TpDer2hJXw";
+const checkRequestStatus = require("./checkResponseStatus");
 
-const getData = url => {
+const getData = (url, credentials) => {
   console.log("you're getting data");
-  fetch(url, {
+  console.log("url:", url);
+  return fetch(url, {
     method: "GET",
     headers: {
       Authorization: `Bearer ${credentials}`
     }
   })
     .then(response => checkRequestStatus(response))
-    .then(response => response.json())
-    .then(data => {
-      console.log("this should be data", data);
-      {
-        this.setState(
-          {
-            commitInfo: data.data
-          },
-          () => {
-            console.log("this is new state", this.state.commitInfo);
-          }
-        );
-      }
-    });
+    .then(response => response.json());
 };
 
 module.exports = getData;
