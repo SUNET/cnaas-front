9772b38a5d4c2abf8f99ca19f15b7c1defe78cd0
Nathalie Jonsson <njons@sunet.se>
2019-11-25 22:11:41 +0100

    imports get and put requests from utils folder

--- react-front/public/components/Workflow_step1.js
+++ react-front/public/components/Workflow_step1.js
@@ -1,5 +1,6 @@
 import React from "react";
-import checkResponseStatus from "../utils/checkResponseStatus";
+import getData from "../utils/getData";
+import { putData } from "../utils/sendData";
 
 class Workflow_step1 extends React.Component {
   state = {
@@ -10,65 +11,53 @@ class Workflow_step1 extends React.Component {
   };
 
   getCommitInfo = () => {
+    let url =
+      "https://tug-lab.cnaas.sunet.se:8443/api/v1.0/repository/settings";
     const credentials =
       "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpYXQiOjE1NzEwNTk2MTgsIm5iZiI6MTU3MTA1OTYxOCwianRpIjoiNTQ2MDk2YTUtZTNmOS00NzFlLWE2NTctZWFlYTZkNzA4NmVhIiwic3ViIjoiYWRtaW4iLCJmcmVzaCI6ZmFsc2UsInR5cGUiOiJhY2Nlc3MifQ.Sfffg9oZg_Kmoq7Oe8IoTcbuagpP6nuUXOQzqJpgDfqDq_GM_4zGzt7XxByD4G0q8g4gZGHQnV14TpDer2hJXw";
-    console.log("you clicked the get commit info button");
-    fetch("https://tug-lab.cnaas.sunet.se:8443/api/v1.0/repository/settings", {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${credentials}`
+
+    getData(url, credentials).then(data => {
+      console.log("this should be data", data);
+      {
+        this.setState(
+          {
+            commitInfo: data.data
+          },
+          () => {
+            console.log("this is new state", this.state.commitInfo);
+          }
+        );
       }
-    })
-      .then(response => checkRequestStatus(response))
-      .then(response => response.json())
-      .then(data => {
-        console.log("this should be data", data);
-        {
-          this.setState(
-            {
-              commitInfo: data.data
-            },
-            () => {
-              console.log("this is new state", this.state.commitInfo);
-            }
-          );
-        }
-      });
+    });
   };
 
   // this request takes some time, perhaps work in a "loading..."
   refreshCommitInfo = () => {
     const credentials =
       "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpYXQiOjE1NzEwNTk2MTgsIm5iZiI6MTU3MTA1OTYxOCwianRpIjoiNTQ2MDk2YTUtZTNmOS00NzFlLWE2NTctZWFlYTZkNzA4NmVhIiwic3ViIjoiYWRtaW4iLCJmcmVzaCI6ZmFsc2UsInR5cGUiOiJhY2Nlc3MifQ.Sfffg9oZg_Kmoq7Oe8IoTcbuagpP6nuUXOQzqJpgDfqDq_GM_4zGzt7XxByD4G0q8g4gZGHQnV14TpDer2hJXw";
-    console.log("you clicked the refresh commit info button");
-    fetch("https://tug-lab.cnaas.sunet.se:8443/api/v1.0/repository/settings", {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${credentials}`
-      },
-      body: JSON.stringify({ action: "REFRESH" })
-    })
-      .then(response => checkRequestStatus(response))
-      .then(response => response.json())
-      .then(data => {
-        console.log("this should be data", data);
-        {
-          this.setState(
-            {
-              latestCommitInfo: data.data
-            },
-            () => {
-              console.log("this is new state", this.state.latestCommitInfo);
-            }
-          );
-        }
-      });
+    let url =
+      "https://tug-lab.cnaas.sunet.se:8443/api/v1.0/repository/settings";
+    let dataToSend = { action: "REFRESH" };
+
+    putData(url, credentials, dataToSend).then(data => {
+      console.log("this should be data", data);
+      {
+        this.setState(
+          {
+            latestCommitInfo: data.data
+          },
+          () => {
+            console.log("this is new state", this.state.latestCommitInfo);
+          }
+        );
+      }
+    });
   };
 
   render() {
     let commitInfo = this.state.commitInfo;
     let refreshedCommitInfo = this.state.latestCommitInfo;
+
     return (
       <div className="workflow-container">
         <div className="workflow-container__header">
