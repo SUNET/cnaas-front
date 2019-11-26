520890126e7b8f122cae59ce1e699bdf83bcf5d1
Nathalie Jonsson <njons@sunet.se>
2019-11-26 13:57:24 +0100

    solves merge conflicts pagination

--- react-front/public/components/DeviceList.js
+++ react-front/public/components/DeviceList.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Button, Select, Input, Icon, Pagination } from 'semantic-ui-react'
+import { Button, Select, Input, Icon, Pagination } from "semantic-ui-react";
 import DeviceSearchForm from "./DeviceSearchForm";
-import getData from "../utils/getData";
+import checkResponseStatus from "../utils/checkResponseStatus";
 import DeviceInitForm from "./DeviceInitForm";
 
 class DeviceList extends React.Component {
@@ -31,21 +31,21 @@ class DeviceList extends React.Component {
       newState["filterValue"] = options.filterValue;
     }
     if (options.pageNum !== undefined) {
-      newState['activePage'] = options.pageNum;
+      newState["activePage"] = options.pageNum;
     }
     this.setState(newState);
     return this.getDevicesAPIData(
       newState["sortField"],
       newState["filterField"],
       newState["filterValue"],
-       newState["activePage"]
+      newState["activePage"]
     );
   };
 
   /**
    * Handle sorting on different columns when clicking the header fields
    */
-  sortHeader = (header) => {
+  sortHeader = header => {
     let newState = this.state;
     let sortField = "id";
     const oldValue = this.state[header + "_sort"];
@@ -63,7 +63,7 @@ class DeviceList extends React.Component {
     this.getDevicesData({ sortField: sortField });
     // Close all expanded table rows when resorting the table
     var deviceDetails = document.getElementsByClassName("device_details_row");
-    for(var i = 0; i < deviceDetails.length; i++) {
+    for (var i = 0; i < deviceDetails.length; i++) {
       deviceDetails[i].hidden = true;
     }
   };
@@ -73,13 +73,13 @@ class DeviceList extends React.Component {
   }
 
   readHeaders = response => {
-    const totalCountHeader = response.headers.get('X-Total-Count');
+    const totalCountHeader = response.headers.get("X-Total-Count");
     if (totalCountHeader !== null && !isNaN(totalCountHeader)) {
-      console.log("total: "+totalCountHeader);
-      const totalPages = Math.ceil( totalCountHeader / 10 );
-      this.setState({totalPages: totalPages})
+      console.log("total: " + totalCountHeader);
+      const totalPages = Math.ceil(totalCountHeader / 10);
+      this.setState({ totalPages: totalPages });
     } else {
-      console.log("Could not find X-Total-Count header, only showing one page")
+      console.log("Could not find X-Total-Count header, only showing one page");
     }
     return response;
   };
@@ -91,31 +91,58 @@ class DeviceList extends React.Component {
     // TODO: filterValue should probably be urlencoded?
     let filterParams = "";
     let filterFieldOperator = "";
-    const stringFields = ["hostname", "management_ip", "serial", "ztp_mac", "platform", "vendor", "model", "os_version"];
+    const stringFields = [
+      "hostname",
+      "management_ip",
+      "serial",
+      "ztp_mac",
+      "platform",
+      "vendor",
+      "model",
+      "os_version"
+    ];
     if (filterField != null && filterValue != null) {
       if (stringFields.indexOf(filterField) !== -1) {
         filterFieldOperator = "[contains]";
       }
-      filterParams = "&filter["+filterField+"]"+filterFieldOperator+"="+filterValue;
+      filterParams =
+        "&filter[" +
+        filterField +
+        "]" +
+        filterFieldOperator +
+        "=" +
+        filterValue;
     }
-    const url =
+    fetch(
       "https://tug-lab.cnaas.sunet.se:8443/api/v1.0/devices?sort=" +
-      sortField +
-      filterParams +
-      "&page=" + pageNum + "&per_page=10";
-    getData(url, credentials).then(data => {
-      console.log("this should be data", data);
+        sortField +
+        filterParams +
+        "&page=" +
+        pageNum +
+        "&per_page=10",
       {
-        this.setState(
-          {
-            devicesData: data.data.devices
-          },
-          () => {
-            console.log("this is new state", this.state.devicesData);
-          }
-        );
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${credentials}`
+        }
       }
-    });
+    )
+      .then(response => checkResponseStatus(response))
+      .then(response => this.readHeaders(response))
+      .then(response => response.json())
+      .then(data => {
+        console.log("this should be data", data);
+        {
+          this.setState(
+            {
+              devicesData: data.data.devices
+            },
+            () => {
+              console.log("this is new state", this.state.devicesData);
+            }
+          );
+        }
+      });
   };
 
   /**
@@ -132,7 +159,7 @@ class DeviceList extends React.Component {
 
   pageChange(e, data) {
     // Update active page and then reload data
-    this.setState({ activePage: data.activePage }, () => 
+    this.setState({ activePage: data.activePage }, () =>
       this.getDevicesData({ numPage: data.activePage })
     );
   }
@@ -143,40 +170,83 @@ class DeviceList extends React.Component {
     deviceInfo = devicesData.map((items, index) => {
       let syncStatus = "";
       if (items.synchronized === true) {
-        syncStatus = <td key="2"><Icon name='check' color='green' /></td>;
+        syncStatus = (
+          <td key="2">
+            <Icon name="check" color="green" />
+          </td>
+        );
       } else {
-        syncStatus = <td key="2"><Icon name='delete' color='red' /></td>;
+        syncStatus = (
+          <td key="2">
+            <Icon name="delete" color="red" />
+          </td>
+        );
       }
       let deviceStateExtra = "";
       if (items.state == "DISCOVERED" || items.state == "DHCP_BOOT") {
         deviceStateExtra = <DeviceInitForm deviceId={items.id} />;
       }
-      return ([
+      return [
         <tr key={index} onClick={this.clickRow.bind(this)}>
-          <td key="0"><Icon name='angle down' />{items.hostname}</td>
+          <td key="0">
+            <Icon name="angle down" />
+            {items.hostname}
+          </td>
           <td key="1">{items.device_type}</td>
           {syncStatus}
           <td key="3">{items.id}</td>
         </tr>,
-        <tr key={index+"_content"} colSpan="4" className="device_details_row" hidden>
+        <tr
+          key={index + "_content"}
+          colSpan="4"
+          className="device_details_row"
+          hidden
+        >
           <td>
             <table className="device_details_table">
               <tbody>
-                <tr><td>Description</td><td>{items.description}</td></tr>
-                <tr><td>Management IP</td><td>{items.management_ip}</td></tr>
-                <tr><td>Infra IP</td><td>{items.infra_ip}</td></tr>
-                <tr><td>MAC</td><td>{items.ztp_mac}</td></tr>
-                <tr><td>Vendor</td><td>{items.vendor}</td></tr>
-                <tr><td>Model</td><td>{items.model}</td></tr>
-                <tr><td>OS Version</td><td>{items.os_version}</td></tr>
-                <tr><td>Serial</td><td>{items.serial}</td></tr>
-                <tr><td>State</td><td>{items.state}</td></tr>
+                <tr>
+                  <td>Description</td>
+                  <td>{items.description}</td>
+                </tr>
+                <tr>
+                  <td>Management IP</td>
+                  <td>{items.management_ip}</td>
+                </tr>
+                <tr>
+                  <td>Infra IP</td>
+                  <td>{items.infra_ip}</td>
+                </tr>
+                <tr>
+                  <td>MAC</td>
+                  <td>{items.ztp_mac}</td>
+                </tr>
+                <tr>
+                  <td>Vendor</td>
+                  <td>{items.vendor}</td>
+                </tr>
+                <tr>
+                  <td>Model</td>
+                  <td>{items.model}</td>
+                </tr>
+                <tr>
+                  <td>OS Version</td>
+                  <td>{items.os_version}</td>
+                </tr>
+                <tr>
+                  <td>Serial</td>
+                  <td>{items.serial}</td>
+                </tr>
+                <tr>
+                  <td>State</td>
+                  <td>{items.state}</td>
+                </tr>
               </tbody>
             </table>
             {deviceStateExtra}
           </td>
         </tr>
-      ]);
+      ];
     });
 
     return (
@@ -190,9 +260,24 @@ class DeviceList extends React.Component {
             <table>
               <thead>
                 <tr>
-                  <th onClick={()=>this.sortHeader("hostname")}>Hostname <Icon name='sort' /> <div className="hostname_sort">{this.state.hostname_sort}</div></th>
-                  <th onClick={()=>this.sortHeader("device_type")}>Device type <Icon name='sort' /> <div className="device_type_sort">{this.state.device_type_sort}</div></th>
-                  <th onClick={()=>this.sortHeader("synchronized")}>Sync. status <Icon name='sort' /> <div className="sync_status_sort">{this.state.synchronized_sort}</div></th>
+                  <th onClick={() => this.sortHeader("hostname")}>
+                    Hostname <Icon name="sort" />{" "}
+                    <div className="hostname_sort">
+                      {this.state.hostname_sort}
+                    </div>
+                  </th>
+                  <th onClick={() => this.sortHeader("device_type")}>
+                    Device type <Icon name="sort" />{" "}
+                    <div className="device_type_sort">
+                      {this.state.device_type_sort}
+                    </div>
+                  </th>
+                  <th onClick={() => this.sortHeader("synchronized")}>
+                    Sync. status <Icon name="sort" />{" "}
+                    <div className="sync_status_sort">
+                      {this.state.synchronized_sort}
+                    </div>
+                  </th>
                   <th>id</th>
                 </tr>
               </thead>
@@ -200,7 +285,11 @@ class DeviceList extends React.Component {
             </table>
           </div>
           <div>
-            <Pagination defaultActivePage={1} totalPages={this.state.totalPages} onPageChange={this.pageChange.bind(this)} />
+            <Pagination
+              defaultActivePage={1}
+              totalPages={this.state.totalPages}
+              onPageChange={this.pageChange.bind(this)}
+            />
           </div>
         </div>
       </section>
