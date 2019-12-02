3b88adf983df4e4c65fc247fd0cffcd28b096939
Nathalie Jonsson <njons@sunet.se>
2019-12-02 12:28:51 +0100

    adds force:true to data POSTed to API on click of Force retry button

--- react-front/public/components/ConfigChange/ConfigChangeStep2.js
+++ react-front/public/components/ConfigChange/ConfigChangeStep2.js
@@ -9,15 +9,29 @@ class ConfigChangeStep2 extends React.Component {
       "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpYXQiOjE1NzEwNTk2MTgsIm5iZiI6MTU3MTA1OTYxOCwianRpIjoiNTQ2MDk2YTUtZTNmOS00NzFlLWE2NTctZWFlYTZkNzA4NmVhIiwic3ViIjoiYWRtaW4iLCJmcmVzaCI6ZmFsc2UsInR5cGUiOiJhY2Nlc3MifQ.Sfffg9oZg_Kmoq7Oe8IoTcbuagpP6nuUXOQzqJpgDfqDq_GM_4zGzt7XxByD4G0q8g4gZGHQnV14TpDer2hJXw",
     syncData: [],
     jobsData: [],
-    repeatingData: [] // setInterval ID saved in state to be accessible to all class methods
+    repeatingData: [], // setInterval ID saved in state to be accessible to all class methods
+    dataToSend: { dry_run: true, all: true }
     // errorMessage: ""
   };
 
+  deviceSyncToWithForce = () => {
+    this.setState(
+      {
+        dataToSend: { dry_run: true, all: true, force: true }
+      },
+      () => {
+        this.deviceSyncTo();
+      }
+    );
+  };
+
   deviceSyncTo = () => {
+    // let dataToSend = { dry_run: true, all: true };
+    let dataToSend = this.state.dataToSend;
+    console.log("this is dataToSend", dataToSend);
     const credentials = this.state.token;
     console.log("you clicked the start sync info button");
     let url = "https://tug-lab.cnaas.sunet.se:8443/api/v1.0/device_syncto";
-    let dataToSend = { dry_run: true, all: true };
     postData(url, credentials, dataToSend).then(data => {
       console.log("this should be data", data);
       {
@@ -106,15 +120,15 @@ class ConfigChangeStep2 extends React.Component {
     }
     // allow a force retry of dry run if it errored
     // jobStatus === "EXCEPTION"
-    if (jobStatus === "FINISHED") {
+    if (true) {
       console.log("jobStatus errored");
       error = [
         <div>
-          <p key="0">something went wrong</p>
-          <button key="1" onClick={this.syncStatus}>
+          <p>something went wrong</p>
+          <button key="0" onClick={this.deviceSyncTo}>
             Retry
           </button>
-          <button key="2" onClick={this.syncStatus({force: true})}>
+          <button key="1" onClick={this.deviceSyncToWithForce}>
             Force retry
           </button>
         </div>
@@ -123,13 +137,13 @@ class ConfigChangeStep2 extends React.Component {
 
     return (
       <div className="workflow-container">
-        <div key="0" className="workflow-container__header">
+        <div className="workflow-container__header">
           <h2>Dry run (2/4)</h2>
           <a href="#">
             <button className="workflow-container__button--hide">Close</button>
           </a>
         </div>
-        <div key="1" className="workflow-collapsable">
+        <div className="workflow-collapsable">
           <p>
             Step 2 of 4: Sending generated configuration to devices to calculate
             diff and check sanity
@@ -142,18 +156,19 @@ class ConfigChangeStep2 extends React.Component {
             <p>{syncStatus}</p>
             <p>{syncJobId}</p>
           </div>
-          <div key="2">
+          <div>
             <ConfigChangeProgressBar
               finishedDevices={finishedDevices}
               totalDevices={totalDevices}
             />
           </div>
-          <div key="3">
+          <div>
             <p>status: {syncStatus}</p>
             <p>start time: {jobStartTime}</p>
             <p>finish time: {jobFinishTime}</p>
           </div>
-          <div key="4">{error}</div>
+
+          <div>{error}</div>
         </div>
       </div>
     );
