094500c62177c1630703f4e0857f88e148bed0b0
Nathalie Jonsson <njons@sunet.se>
2019-12-08 15:45:57 +0100

    adds in full details needed in state and defines dryRunResult variable

--- react-front/public/components/ConfigChange/ConfigChange.js
+++ react-front/public/components/ConfigChange/ConfigChange.js
@@ -12,7 +12,9 @@ class ConfigChange extends React.Component {
     token:
       "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpYXQiOjE1NzEwNTk2MTgsIm5iZiI6MTU3MTA1OTYxOCwianRpIjoiNTQ2MDk2YTUtZTNmOS00NzFlLWE2NTctZWFlYTZkNzA4NmVhIiwic3ViIjoiYWRtaW4iLCJmcmVzaCI6ZmFsc2UsInR5cGUiOiJhY2Nlc3MifQ.Sfffg9oZg_Kmoq7Oe8IoTcbuagpP6nuUXOQzqJpgDfqDq_GM_4zGzt7XxByD4G0q8g4gZGHQnV14TpDer2hJXw",
     dryRunSyncData: [],
-    dryRunProgressData: []
+    dryRunSyncJobid: null,
+    dryRunProgressData: [],
+    dryRunResultData: []
     // errorMessage: ""
   };
 
@@ -73,6 +75,7 @@ class ConfigChange extends React.Component {
   render() {
     let dryRunProgressData = this.state.dryRunProgressData;
     let dryRunJobStatus = "";
+    let dryRunResults = "";
     dryRunProgressData.map((job, i) => {
       dryRunJobStatus = job.status;
     });
