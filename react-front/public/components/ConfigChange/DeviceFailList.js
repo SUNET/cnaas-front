cb2a284a86b9c6911f95c0e4632898bd61fa5b64
Nathalie Jonsson <njons@sunet.se>
2019-12-08 17:39:31 +0100

    adds function that iterates through the values of each device and creates a new array based on the failed status of each device. If failed: true the index value of the device is added to the new array, else false is added to the new array

--- react-front/public/components/ConfigChange/DeviceFailList.js
+++ react-front/public/components/ConfigChange/DeviceFailList.js
@@ -4,7 +4,21 @@ class DeviceFailList extends React.Component {
   render() {
     let failedDeviceNameList = "";
     let devicesObj = this.props.devices;
-    console.log("this is devicesObj", devicesObj);
+    // console.log("this is devicesObj", devicesObj);
+    // split device object up into a key array and a values array
+    const deviceNames = Object.keys(devicesObj);
+    const deviceData = Object.values(devicesObj);
+    console.log("this deviceData", deviceData);
+
+    const failedDeviceNameObj = deviceData
+      .map((status, i) => {
+        if (status.failed === true) {
+          return i;
+        }
+        return false;
+      });
+    
+      console.log("failedDeviceNameObj: ", failedDeviceNameObj);
 
     return (
       <div>
