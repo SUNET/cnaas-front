66de3ba0d0f4b3d1b7958d9b03b42fee4891c0c0
Johan Marcusson <johan.marcusson@sunet.se>
2020-01-15 10:51:16 +0100

    Move files around

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from "./public/components/App";
+import App from "./components/App";
 // const App = () => <h1>Hello World</h1>;
 
 render(<App />, document.getElementById('root'));
