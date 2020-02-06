e9eeb9e851843a03d0ed2af6f3424072790ab3b6
Nathalie Jonsson <njons@sunet.se>
2020-02-06 12:39:32 +0100

    final clean of comented out code

--- public/components/ConfigChange/DryRun/DryRunProgressBar.js
+++ public/components/ConfigChange/DryRun/DryRunProgressBar.js
@@ -2,9 +2,6 @@ import React from "react";
 import ProgressBar from "../../ProgressBar";
 
 class DryRunProgressBar extends React.Component {
-  // randomIntFromInterval(min, max) {
-  //   return Math.floor(Math.random() * (max - min + 1) + min);
-  // }
   render() {
     let progressData = this.props.dryRunProgressData;
     let jobStatus = this.props.dryRunJobStatus;
@@ -18,13 +15,10 @@ class DryRunProgressBar extends React.Component {
       progressData.map((job, i) => {
         finishedDevicesData = job.finished_devices;
         finishedDevicesNum = finishedDevicesData.length;
-        console.log("finishedDevicesData :", finishedDevicesData);
-        console.log("finishedDevicesNum:", finishedDevicesNum);
+        // console.log("finishedDevicesData :", finishedDevicesData);
+        // console.log("finishedDevicesNum:", finishedDevicesNum);
         percentageValue = (finishedDevicesNum / totalDevices) * 100;
-        console.log("this is the percentage value:", percentageValue);
-        // if (jobStatus === "FINISHED") {
-        //   percentageValue = percentageValue / 100;
-        // }
+        // console.log("this is the percentage value:", percentageValue);
       });
     }
 
