3a23eff346fe91abb65f0f9eace80a72db191a98
Johan Marcusson <johan.marcusson@sunet.se>
2020-04-22 16:41:36 +0200

    Add log window to progress info

--- public/components/ConfigChange/DryRun/DryRunProgressInfo.js
+++ public/components/ConfigChange/DryRun/DryRunProgressInfo.js
@@ -1,6 +1,29 @@
 import React from "react";
+const io = require("socket.io-client");
 
 class DryRunProgressInfo extends React.Component {
+  state = {
+    socket: null,
+    logLines: []
+  };
+
+  componentDidMount(){
+    const socket = io(process.env.API_URL);
+    socket.on('connect', function(data) {
+      console.log('Websocket connected!');
+      var ret = socket.emit('logs', {'level': 'DEBUG'});
+      console.log(ret)  
+    });
+    socket.on('cnaas_log', (data) => {
+      console.log(data);
+      var newLogLines = this.state.logLines;
+      newLogLines.push(data + "\n");
+      this.setState({logLines: newLogLines});
+      var element = document.getElementById("logoutputdiv");
+      element.scrollTop = element.scrollHeight;
+    });
+  };
+
   render() {
     // console.log("this is props in configchange progress data", this.props);
     let progressData = this.props.dryRunProgressData;
@@ -9,6 +32,10 @@ class DryRunProgressInfo extends React.Component {
     let jobStartTime = "";
     let jobFinishTime = "";
     let exceptionMessage = "";
+    let log = "";
+    this.state.logLines.map((logLine) => {
+      log = log + logLine;
+    })
 
     progressData.map((job, i) => {
       jobStartTime = job.start_time;
@@ -24,6 +51,11 @@ class DryRunProgressInfo extends React.Component {
         <p className="error">{exceptionMessage}</p>
         <p>start time: {jobStartTime}</p>
         <p>finish time: {jobFinishTime}</p>
+        <div id="logoutputdiv" className="logoutput">
+          <pre>
+            {log}
+          </pre>
+        </div>
       </div>
     );
   }
