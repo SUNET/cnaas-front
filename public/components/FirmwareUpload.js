0fcea76b4573f4516187673fa8f14cecf8fdc236
Johan Marcusson <johan.marcusson@sunet.se>
2020-11-13 16:13:59 +0100

    Get firmware data from central repo and from nms and populate table. Show icons and data field describing status of the firmwares.

--- public/components/FirmwareUpload.js
+++ public/components/FirmwareUpload.js
@@ -1,39 +1,47 @@
 import React from "react";
 import formatISODate from "../utils/formatters";
 import checkResponseStatus from "../utils/checkResponseStatus";
-import { Icon } from "semantic-ui-react";
+import { Icon, Button, Popup } from "semantic-ui-react";
+import getData from "../utils/getData";
 
 class FirmwareUpload extends React.Component {
   state = {
-    firmwareRepoData: []
+    firmwareRepoData: [],
+    firmwareNmsData: []
   }
 
   getFirmwareRepoData = () => {
-    fetch(
-      "https://firmware.cnaas.sunet.se/firmware.json",
+    let url = "https://firmware.cnaas.sunet.se/firmware.json";
+    getData(url).then(data => {
+      console.log("this should be REPO data", data);
       {
-        method: "GET",
+        this.setState(
+          {
+            firmwareRepoData: data.firmwares
+          }
+        );
       }
-    )
-      .then(response => checkResponseStatus(response))
-      .then(response => response.json())
-      .then(data => {
-        console.log("this should be data", data);
-        {
-          this.setState(
-            {
-              firmwareRepoData: data.firmwares
-            },
-            () => {
-              console.log("this is new state", this.state.firmwareRepoData);
-            }
-          );
-        }
-      });
+    });
   };
+
+  getFirmwareFiles() {
+    const credentials = localStorage.getItem("token");
+    let url = process.env.API_URL + "/api/v1.0/firmware";
+    getData(url, credentials).then(data => {
+      console.log("this should be NMS data", data);
+      {
+        this.setState(
+          {
+            firmwareNmsData: data.data.files
+          }
+        );
+      }
+    });
+  }
 
   componentDidMount() {
     this.getFirmwareRepoData();
+    this.getFirmwareFiles();
   }
 
   clickRow(e) {
@@ -56,14 +64,75 @@ class FirmwareUpload extends React.Component {
   }
 
   render() {
-    let firmwareData = this.state.firmwareRepoData;
+    let firmwareNmsData = this.state.firmwareNmsData;
+    let firmwareData = this.state.firmwareRepoData.map((repoFirmware, index) => {
+      let newRepoFirmware = repoFirmware;
+      newRepoFirmware["present_in_repo"] = true;
+      let popIndex = firmwareNmsData.indexOf(repoFirmware.filename);
+      if (popIndex > -1) {
+        newRepoFirmware["already_uploaded"] = true;
+        firmwareNmsData.splice(popIndex, 1);
+      } else {
+        newRepoFirmware["already_uploaded"] = false;
+      }
+      return newRepoFirmware;
+    });
+    console.log("matched firmwares: ", firmwareData);
+    firmwareNmsData.map((firmware) => {
+      firmwareData.push({
+        filename: firmware,
+        approved: false,
+        present_in_repo: false,
+        approved_by: "",
+        approved_date: "",
+        end_of_life_date: "",
+        already_uploaded: true
+      });
+    })
+    console.log("unmatched firmwares: ", firmwareNmsData);
 
     let firmwareTableBody = firmwareData.map((firmware, index) => {
+      let ico = [];
+      if (firmware.present_in_repo) {
+        ico.push(<Popup
+                   content="This firmware is present in the central firmware repository"
+                   position="top center"
+                   trigger={<Icon name="cloud" />} />);
+      }
+      if (firmware.already_uploaded) {
+        ico.push(<Popup
+                   content="This firmware is present on this local NMS instance"
+                   position="top center"
+                   trigger={<Icon name="disk" />} />);
+      }
+      if (firmware.approved) {
+        ico.push(<Popup
+                   content="This firmware is verified/approved"
+                   position="top center"
+                   trigger={<Icon name="check" color="green" />} />);
+      } else {
+        ico.push(<Popup
+                   content="Warning! This firmware is not verified/approved"
+                   position="top center"
+                   trigger={<Icon name="delete" color="red" />} />);
+      }
+
+      let actions = [];
+      if (firmware.already_uploaded) {
+        actions.push(
+          <form><Button>Remove <Icon name="trash alternate outline" /></Button></form>
+        );
+      } else {
+        actions.push(
+          <form><Button>Upload</Button></form>
+        );
+      }
       return [
         <tr key={index} onClick={this.clickRow.bind(this)}>
           <td key="0">
             <Icon name="angle right" />
             {firmware.filename}
+            {ico}
           </td>
         </tr>,
         <tr
@@ -80,19 +149,20 @@ class FirmwareUpload extends React.Component {
                   <td>{firmware.filename}</td>
                 </tr>
                 <tr>
-                  <td>Finish time</td>
+                  <td>Approved by</td>
                   <td>{firmware.approved_by}</td>
                 </tr>
                 <tr>
-                  <td>Comment</td>
+                  <td>Approved date</td>
                   <td>{firmware.approved_date}</td>
                 </tr>
                 <tr>
-                  <td>Ticket reference</td>
+                  <td>End of life date</td>
                   <td>{firmware.end_of_life_date}</td>
                 </tr>
               </tbody>
             </table>
+            {actions}
           </td>
         </tr>
       ];
