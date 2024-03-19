SUNET/cnaas-front

commit 7cd1110e9991c4b6943fe5ab628a60a9c7b92091

Improved responses when error

public/components/DeviceList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Dropdown, Icon, Pagination, Popup, Button, Select, Checkbox, Modal, Input } from "semantic-ui-react";
 import DeviceSearchForm from "./DeviceSearchForm";
-import checkResponseStatus from "../utils/checkResponseStatus";
+import checkJsonResponse from "../utils/checkJsonResponse";
 import DeviceInitForm from "./DeviceInitForm";
 import queryString from 'query-string';
 import getData from "../utils/getData";
@@ -380,9 +380,7 @@ class DeviceList extends React.Component {
         }
       }
     )
-    .then(response => checkResponseStatus(response))
-    .then(response => this.readHeaders(response))
-    .then(response => response.json())
+    .then(response => checkJsonResponse(response))
     .then(data => {
       console.log("this should be data", data);
       {

public/components/JobList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button, Select, Input, Icon, Pagination, TableBody } from "semantic-ui-react";
-import checkResponseStatus from "../utils/checkResponseStatus";
+import checkJsonResponse from "../utils/checkJsonResponse";
 import JobSearchForm from "./JobSearchForm";
 import VerifyDiffResult from "./ConfigChange/VerifyDiff/VerifyDiffResult";
 import formatISODate from "../utils/formatters";
@@ -184,9 +184,7 @@ class JobList extends React.Component {
         }
       }
     )
-      .then(response => checkResponseStatus(response))
-      .then(response => this.readHeaders(response))
-      .then(response => response.json())
+      .then(response => checkJsonResponse(response))
       .then(data => {
         console.log("this should be data", data);
         {

public/components/GroupList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import checkResponseStatus from "../utils/checkResponseStatus";
+import checkJsonResponse from "../utils/checkJsonResponse";
 import { Icon } from 'semantic-ui-react'
 
 class GroupList extends React.Component {
@@ -27,8 +27,7 @@ class GroupList extends React.Component {
         }
       }
     )
-    .then(response => checkResponseStatus(response))
-    .then(response => response.json())
+    .then(response => checkJsonResponse(response))
     .then(data => {
       console.log("this should be data", data);
       {

commit c1b2bfb09c85c5d3f3da0e7bd6caa9b3a64a3a95

fix some pagination errors introduced in 7cd1110e9991c4b6943fe5ab628a60a9c7b92091

public/components/DeviceList.js
@@ -381,6 +381,7 @@ class DeviceList extends React.Component {
         }
       }
     )
+    .then(response => this.readHeaders(response))
     .then(response => checkJsonResponse(response))
     .then(data => {
       console.log("this should be data", data);

public/components/JobList.js
@@ -184,6 +184,7 @@ class JobList extends React.Component {
         }
       }
     )
+      .then(response => this.readHeaders(response))
       .then(response => checkJsonResponse(response))
       .then(data => {
         console.log("this should be data", data);