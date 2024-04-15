0f5912bb191730dd6982215e1deda0c03359bc05
Johannes Jeppsson <2286858+YotaYota@users.noreply.github.com>
2024-04-15 13:31:39 +0200

    set up react testing library with jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+const config = {
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
+  },
+  setupFilesAfterEnv: ["<rootDir>/public/setupTests.js"],
+};
+
+module.exports = config;
