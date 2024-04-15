b07a3940c7cf80428c2210d6a7318bb2b1ec6bc9
Johannes Jeppsson <2286858+YotaYota@users.noreply.github.com>
2024-04-15 13:33:01 +0200

    test ConfigChangeStep1

---
+++ public/components/ConfigChange/ConfigChangeStep1.test.js
@@ -0,0 +1,102 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+
+import ConfigChangeStep1 from "./ConfigChangeStep1";
+
+import { getData as mockGetData } from "../../utils/getData";
+import { putData as mockPutData } from "../../utils/sendData";
+
+jest.mock("../../utils/getData");
+jest.mock("../../utils/sendData");
+mockGetData.mockResolvedValue({ data: "repo_name_mock" });
+mockPutData.mockResolvedValue(true);
+
+beforeEach(() => {
+  mockGetData.mockClear();
+  mockPutData.mockClear();
+  jest
+    .spyOn(Storage.prototype, "getItem")
+    .mockResolvedValue("localStorageMockValue");
+});
+
+afterAll(() => {
+  global.Storage.prototype.getItem.mockReset();
+});
+
+test("loads and displays 3 enabled buttons", async () => {
+  render(<ConfigChangeStep1 setRepoWorking={jest.fn()} />);
+  const buttons = screen.getAllByRole("button");
+
+  expect(buttons.length).toBe(3);
+  buttons.forEach((button) => expect(button).toBeEnabled());
+});
+
+test("click refresh settings success", async () => {
+  mockPutData.mockResolvedValueOnce({
+    status: "success",
+    data: "repo name mock",
+  });
+  render(<ConfigChangeStep1 setRepoWorking={jest.fn()} />);
+
+  const refreshSettingsButton = screen.getByRole("button", {
+    name: "Refresh settings",
+  });
+  await userEvent.click(refreshSettingsButton);
+
+  expect(screen.getByText("success")).toBeInTheDocument();
+  expect(screen.getByText("repo name mock")).toBeInTheDocument();
+});
+
+test("click refresh settings not success", async () => {
+  mockPutData.mockResolvedValueOnce({
+    status: "not_success",
+    message: "A message recieved",
+  });
+  render(<ConfigChangeStep1 setRepoWorking={jest.fn()} />);
+
+  const refreshSettingsButton = screen.getByRole("button", {
+    name: "Refresh settings",
+  });
+  await userEvent.click(refreshSettingsButton);
+
+  expect(screen.getByText("error")).toBeInTheDocument();
+  expect(screen.getByText("A message recieved")).toBeInTheDocument();
+});
+
+test("click refresh and dry run", async () => {
+  mockPutData.mockResolvedValueOnce({
+    status: "success",
+    data: "repo name mock",
+  });
+  render(
+    <ConfigChangeStep1 setRepoWorking={jest.fn()} onDryRunReady={jest.fn()} />,
+  );
+
+  const refreshAndDryRunButton = screen.getByRole("button", {
+    name: "Refresh settings + dry run",
+  });
+  await userEvent.click(refreshAndDryRunButton);
+
+  expect(screen.getByText("success")).toBeInTheDocument();
+  expect(screen.getByText("repo name mock")).toBeInTheDocument();
+});
+
+test("click refresh templates", async () => {
+  mockPutData.mockResolvedValueOnce({
+    status: "success",
+    data: "repo name mock",
+  });
+  render(
+    <ConfigChangeStep1 setRepoWorking={jest.fn()} onDryRunReady={jest.fn()} />,
+  );
+
+  const refreshTemplatesButton = screen.getByRole("button", {
+    name: "Refresh templates",
+  });
+  await userEvent.click(refreshTemplatesButton);
+
+  expect(screen.getByText("success")).toBeInTheDocument();
+  expect(screen.getByText("repo name mock")).toBeInTheDocument();
+});
