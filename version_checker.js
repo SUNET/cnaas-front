a6872e3e3c665e499685779496d8072f81e32832
Benedith Mulongo <benedith@sunet.se>
2024-07-05 11:00:31 +0200

    Improve style

--- version_checker.js
+++ version_checker.js
@@ -1,4 +1,4 @@
-import {data} from '/dist/version.js'
+import { data } from "/dist/version.js";
 
 async function getData() {
   const url = `${process.env.API_URL}/api/v1.0/system/version`;
@@ -11,29 +11,31 @@ async function getData() {
 }
 
 async function gatherData() {
-    const version = data.version;
-    const dataFrGit = await getData();
-    const gitVersion = dataFrGit.version;
-    const gitVersionSummary = dataFrGit.git_version;
-    console.log("Data:")
-    console.log(version)
-    
-    console.log("dataFrGit:")
-    console.log(dataFrGit)
-    console.log(gitVersion)
-    console.log(gitVersionSummary)
-    console.log(gitVersionSummary.includes(version))
-    if(!gitVersionSummary.includes(version)) {
-        window.dispatchEvent(new CustomEvent('versionchanged', { detail: version }));
-    }
+  const version = data.version;
+  const dataFrGit = await getData();
+  const gitVersion = dataFrGit.version;
+  const gitVersionSummary = dataFrGit.git_version;
+  console.log("Data:");
+  console.log(version);
+
+  console.log("dataFrGit:");
+  console.log(dataFrGit);
+  console.log(gitVersion);
+  console.log(gitVersionSummary);
+  console.log(gitVersionSummary.includes(version));
+  if (!gitVersionSummary.includes(version)) {
+    window.dispatchEvent(
+      new CustomEvent("versionchanged", { detail: version }),
+    );
+  }
 }
 
 function start() {
-    setInterval(gatherData, 8000);
+  setInterval(gatherData, 8000);
 }
 
-window.addEventListener('versionchanged', function (e) {
-    alert('Unmacthed version. New UI version available!');
+window.addEventListener("versionchanged", function (e) {
+  alert("Unmacthed version. New UI version available!");
 });
 
-window.addEventListener('load', start);
+window.addEventListener("load", start);
