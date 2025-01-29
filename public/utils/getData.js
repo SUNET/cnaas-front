a5c42739083f23e444ef783bb07d59d0f7823689
Johan Marcusson <johan.marcusson@sunet.se>
2025-01-29 12:12:39 +0100

    Add separate functions for getData with custom headers

--- public/utils/getData.js
+++ public/utils/getData.js
@@ -11,6 +11,26 @@ const getData = (url, credentials, signal) => {
   }).then((response) => checkJsonResponse(response));
 };
 
+const getDataHeaders = (url, credentials, headers) => {
+  return fetch(url, {
+    method: "GET",
+    headers: {
+      ...(credentials && { Authorization: `Bearer ${credentials}` }),
+      ...(headers && headers),
+    },
+  }).then((response) => checkJsonResponse(response));
+};
+
+const getDataToken = (url, credentials, signal) => {
+  return fetch(url, {
+    method: "GET",
+    headers: {
+      ...(credentials && { Authorization: `Token ${credentials}` }),
+    },
+    ...(signal && { signal }),
+  }).then((response) => checkJsonResponse(response));
+};
+
 const getResponse = (url, credentials) => {
   if (credentials !== undefined) {
     return fetch(url, {
@@ -26,4 +46,4 @@ const getResponse = (url, credentials) => {
   }).then((response) => checkResponseStatus(response));
 };
 
-module.exports = { getData, getResponse };
+module.exports = { getData, getDataHeaders, getDataToken, getResponse };
