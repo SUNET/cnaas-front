e892bdf1be98423c703a9c6dfc19d6483523c0c2
Johannes Jeppsson <johannes@sunet.se>
2025-01-30 14:03:43 +0100

    refactor ProgressBar to func

--- public/components/ProgressBar.js
+++ public/components/ProgressBar.js
@@ -1,58 +1,54 @@
 import React from "react";
 import { Progress } from "semantic-ui-react";
-import PropTypes from "prop-types";
 
-class ProgressBar extends React.Component {
-  render() {
-    let active = false;
-    let disabled = true;
-    let success = false;
-    let error = false;
-    if (this.props.jobStatus !== undefined) {
-      if (this.props.jobStatus === "SCHEDULED") {
+function ProgressBar({ jobStatus, value, total, hidden }) {
+  let active = false;
+  let disabled = true;
+  let success = false;
+  let error = false;
+
+  if (jobStatus !== undefined) {
+    switch (jobStatus) {
+      case "SCHEDULED":
         disabled = false;
-      } else if (this.props.jobStatus === "RUNNING") {
+        break;
+      case "RUNNING":
         disabled = false;
         active = true;
-      } else if (this.props.jobStatus === "FINISHED") {
-        if (this.props.value == this.props.total) {
+        break;
+      case "FINISHED":
+        if (value == total) {
           success = true;
         } else {
           error = true;
         }
-      } else if (this.props.jobStatus === "EXCEPTION") {
+        break;
+      case "EXCEPTION":
         error = true;
-      }
+        break;
+      default:
+        console.error(`unrecognized job status: ${jobStatus}`);
     }
-
-    return (
-      <div>
-        <div id="progressbar" hidden={this.props.hidden}>
-          <Progress
-            value={this.props.value}
-            total={this.props.total}
-            progress
-            precision={0}
-            color="orange"
-            disabled={disabled}
-            active={active}
-            success={success}
-            error={error}
-          />
-          <label>
-            {this.props.value}/{this.props.total} devices finished
-          </label>
-        </div>
-      </div>
-    );
   }
-}
 
-ProgressBar.propTypes = {
-  value: PropTypes.number,
-  total: PropTypes.number,
-  jobStatus: PropTypes.string,
-  hidden: PropTypes.bool,
-};
+  return (
+    <div id="progressbar" hidden={hidden}>
+      <Progress
+        value={value}
+        total={total}
+        progress
+        precision={0}
+        color="orange"
+        disabled={disabled}
+        active={active}
+        success={success}
+        error={error}
+      />
+      <label>
+        {value}/{total} devices finished
+      </label>
+    </div>
+  );
+}
 
 export default ProgressBar;
