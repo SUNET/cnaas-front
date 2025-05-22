ac3c08856ff2e171a27fc04de455a7935b1251e5
Johannes Jeppsson <johannes@sunet.se>
2025-05-22 15:05:44 +0200

    parcel2 with jest and babel fixes

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,18 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
 const config = {
+  clearMocks: true,
   testEnvironment: "jsdom",
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/__mocks__/fileMock.js",
     "\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
   },
-  setupFilesAfterEnv: ["<rootDir>/public/setupTests.js"],
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
 };
 
 module.exports = config;
