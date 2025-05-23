b9ec7f4913aab8a1fe126d5a558d82a41c0ce783
Johannes Jeppsson <johannes@sunet.se>
2025-05-23 10:55:40 +0200

    mv App.js to src folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import AuthContextProvider from "../contexts/AuthContext";
-import { Footer } from "./Footer";
-import Header from "./Header/Header";
-import Panel from "./Panel";
+import AuthContextProvider from "./contexts/AuthContext";
+import { Footer } from "./components/Footer";
+import Header from "./components/Header/Header";
+import Panel from "./components/Panel";
 // needed for routing
-import "../styles/reset.css";
-import "../styles/main.css";
+//import "./styles/reset.css";
+//import "./styles/main.css";
 
 function App() {
   return (
