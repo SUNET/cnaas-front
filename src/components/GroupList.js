9e82e1985aba23c2be962cb953b441b7462869c8
Jakob Mellberg <jakob.mellberg@oru.se>
2025-10-16 12:20:34 +0200

    Removed unused divs, extracted more code into separate components

--- src/components/GroupList.js
+++ src/components/GroupList.js
@@ -1,72 +1,49 @@
+import PropTypes from "prop-types";
 import { useEffect, useState } from "react";
-import { Icon } from "semantic-ui-react";
+import { Icon, Loader } from "semantic-ui-react";
 import { useAuthToken } from "../contexts/AuthTokenContext";
 import { getData } from "../utils/getData";
 import permissionsCheck from "../utils/permissions/permissionsCheck";
 
-function GroupTableBody() {
-  const [groupData, setGroupData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+function GroupLoading() {
+  return (
+    <tbody>
+      <tr key="Loading">
+        <td colSpan="5">
+          <Loader active inline="centered">
+            Loading groups...{" "}
+          </Loader>
+        </td>
+      </tr>
+    </tbody>
+  );
+}
 
-  const { token } = useAuthToken();
+function GroupError({ message }) {
+  return (
+    <tbody>
+      <tr key="error">
+        <td colSpan="5">API error: {message}</td>
+      </tr>
+    </tbody>
+  );
+}
 
-  useEffect(() => {
-    getGroupsData();
-  }, []);
+GroupError.propTypes = {
+  message: PropTypes.string,
+};
 
-  const getGroupsData = async () => {
-    setLoading(true);
-    setError(null);
+function GroupEmptyResult() {
+  return (
+    <tbody>
+      <tr>
+        <td colSpan="5">Empty result</td>
+      </tr>
+    </tbody>
+  );
+}
 
-    try {
-      // Build filter part of the URL to only return specific devices from the API
-      // TODO: filterValue should probably be urlencoded?
-      const data = await getData(
-        `${process.env.API_URL}/api/v1.0/groups`,
-        token,
-      );
-      setGroupData(data.data.groups);
-      setLoading(false);
-    } catch (error) {
-      setGroupData([]);
-      setError(error);
-      setLoading(false);
-    }
-  };
-  if (loading) {
-    return (
-      <tbody>
-        <tr key="Loading">
-          <td colSpan="5">
-            <Icon name="spinner" loading />
-            Loading groups...
-          </td>
-        </tr>
-      </tbody>
-    );
-  }
-
-  if (error) {
-    return (
-      <tbody>
-        <tr key="error">
-          <td colSpan="5">API error: {error.message}</td>
-        </tr>
-      </tbody>
-    );
-  }
-
-  if (groupData.length === 0) {
-    return (
-      <tbody>
-        <tr>
-          <td colSpan="5">Empty result</td>
-        </tr>
-      </tbody>
-    );
-  }
-
+function GroupResult({ groupData }) {
   return (
     <tbody>
       {Object.entries(groupData).map(([group, devices]) => (
@@ -103,25 +80,68 @@ function GroupTableBody() {
   );
 }
 
+GroupResult.propTypes = {
+  groupData: PropTypes.objectOf(PropTypes.arrayOf(PropTypes.string)).isRequired,
+};
+
+function GroupTableBody() {
+  const [groupData, setGroupData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const { token } = useAuthToken();
+
+  useEffect(() => {
+    getGroupsData();
+  }, []);
+
+  const getGroupsData = async () => {
+    setError(null);
+    try {
+      // Build filter part of the URL to only return specific devices from the API
+      // TODO: filterValue should probably be urlencoded?
+      const data = await getData(
+        `${process.env.API_URL}/api/v1.0/groups`,
+        token,
+      );
+      setGroupData(data.data.groups);
+      setLoading(false);
+    } catch (error) {
+      setGroupData([]);
+      setError(error);
+      setLoading(false);
+    }
+  };
+
+  if (loading) {
+    return <GroupLoading />;
+  }
+
+  if (error) {
+    return <GroupError message={error.message} />;
+  }
+
+  if (groupData.length === 0) {
+    return <GroupEmptyResult />;
+  }
+
+  return <GroupResult groupData={groupData} />;
+}
+
 function GroupList() {
   return (
     <section>
-      <div id="group_list">
-        <h2>Group list</h2>
-        <div id="data">
-          <table>
-            <thead>
-              <tr>
-                <th>Group name</th>
-                <th>Group members</th>
-                <th hidden={!permissionsCheck("Groups", "read")}>Actions</th>
-              </tr>
-            </thead>
-            <GroupTableBody />
-          </table>
-        </div>
-        <div />
-      </div>
+      <h2>Group list</h2>
+      <table id="data">
+        <thead>
+          <tr>
+            <th>Group name</th>
+            <th>Group members</th>
+            <th hidden={!permissionsCheck("Groups", "read")}>Actions</th>
+          </tr>
+        </thead>
+        <GroupTableBody />
+      </table>
     </section>
   );
 }
