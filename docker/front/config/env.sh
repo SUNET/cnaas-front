b669bffefeadfd671a55ee1be62580b6af519b2f
Jakob Mellberg <jakob.mellberg@oru.se>
2025-11-17 16:25:02 +0100

    Workaround to get .env variables working in the frontend dynamically

--- docker/front/config/env.sh
+++ docker/front/config/env.sh
@@ -31,20 +31,18 @@ if [ -z "$GRAPHITE_URL" ]; then
     sed -i "/location.*graphite/,/}/d" /etc/nginx/conf.d/default.conf
 fi
 
+# Update nginx with dynamic variables
 for key in $VARS; do
     value=$(printenv "$key")
     if [ -n "$value" ]; then
         echo "Found $key"
-        # JS update
-        find /opt/cnaas/static -type f -name '*.js' -exec \
-            sed -i "s|${key}|${value}|g" '{}' +
         # Update nginx
         sed -i "s|${key}|${value}|g" /etc/nginx/conf.d/default.conf
     else
         echo "Environment variable $key not set, skipping"
     fi
 done
-echo "Done with setting environment variables in JS-files and nginx"
+echo "Done with setting environment variables in nginx"
 
 if [ ! -f /opt/cnaas/cert/cnaasfront_combined.crt ]; then
   echo "WARNING: no cert found, using snakeoil cert"
@@ -54,6 +52,25 @@ fi
 chown nginx:nginx /opt/cnaas/cert/cnaasfront.key
 chmod 600 /opt/cnaas/cert/cnaasfront.key
 
+# Create config.js dynamically using a here-doc
+cat > "/opt/cnaas/static/config.js" <<EOF
+var API_URL = "${API_URL:-}";
+var AUTH_URL = "${AUTH_URL:-}";
+var NETBOX_API_URL = "${NETBOX_API_URL:-}";
+var NETBOX_TENANT_ID = "${NETBOX_TENANT_ID:-}";
+var FIRMWARE_URL = "${FIRMWARE_URL:-}";
+var TEMPLATES_WEB_URL = "${TEMPLATES_WEB_URL:-}";
+var SETTINGS_WEB_URL = "${SETTINGS_WEB_URL:-}";
+var MONITORING_WEB_URL = "${MONITORING_WEB_URL:-}";
+var FIRMWARE_REPO_METADATA_URL = "${FIRMWARE_REPO_METADATA_URL:-}";
+var FIRMWARE_REPO_URL = "${FIRMWARE_REPO_URL:-}";
+var OIDC_ENABLED = "${OIDC_ENABLED:-}";
+var ARISTA_DETECT_ARCH = "${ARISTA_DETECT_ARCH:-}";
+var GRAPHITE_URL = "${GRAPHITE_URL:-}";
+EOF
 
-
+# Add config.js to index.html
+sed -i '/<script type=module src=\/cnaas.*/a\
+<script src="/config.js"></script>
+' /opt/cnaas/static/index.html
 
