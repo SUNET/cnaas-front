8dfe64231f9e5ec7622485ca16c04efec33c454d
Johannes Jeppsson <johannes@sunet.se>
2025-12-18 08:31:48 +0100

    fix filterAction params

--- src/components/DeviceList/actionModals/HostnameModal.js
+++ src/components/DeviceList/actionModals/HostnameModal.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, Modal, Input, Loader } from "semantic-ui-react";
+import { Button, Modal, Input, Loader, Icon } from "semantic-ui-react";
 import PropTypes from "prop-types";
 import { useAuthToken } from "../../../contexts/AuthTokenContext";
 import { putData } from "../../../utils/sendData";
@@ -7,6 +7,8 @@ import { putData } from "../../../utils/sendData";
 HostnameModal.propTypes = {
   deviceId: PropTypes.number,
   hostname: PropTypes.string,
+  isOpen: PropTypes.bool,
+  closeAction: PropTypes.func,
 };
 
 export function HostnameModal({ deviceId, hostname, isOpen, closeAction }) {
@@ -14,9 +16,12 @@ export function HostnameModal({ deviceId, hostname, isOpen, closeAction }) {
   const [newHostname, setNewHostname] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
+  const [success, setSuccess] = useState(false);
 
   const putHostname = async () => {
     setIsLoading(true);
+    setError("");
+    setSuccess(false);
 
     const url = `${process.env.API_URL}/api/v1.0/device/${deviceId}`;
     const dataToSend = { hostname: newHostname };
@@ -24,10 +29,13 @@ export function HostnameModal({ deviceId, hostname, isOpen, closeAction }) {
       const data = await putData(url, token, dataToSend);
 
       if (data.status !== "success") {
+        setSuccess(false);
         setError(data.error);
       }
+      setSuccess(true);
     } catch (err) {
-      setError(err);
+      setError(err.message);
+      setSuccess(false);
     }
     setIsLoading(false);
   };
@@ -37,16 +45,24 @@ export function HostnameModal({ deviceId, hostname, isOpen, closeAction }) {
       <Modal.Header>Change hostname for {hostname}</Modal.Header>
       <Modal.Content>
         <Modal.Description>
-          Type the new hostname:
+          Type the new hostname:{" "}
           <Input
             type="text"
             placeholder="new hostname..."
-            action
             onChange={(e) => setNewHostname(e.target.value)}
             value={newHostname}
+            fluid
           />
-          {isLoading && <Loader />}
+          {isLoading && <Loader active inline />}
+          {!isLoading && <Loader active inline />}
           {error && <p> {error} </p>}
+          <p>{success && (
+            <>
+              <Icon name="check" color="green" />
+              <label>Hostname changed</label>
+            </>
+          )}
+          </p>
         </Modal.Description>
       </Modal.Content>
       <Modal.Actions>
@@ -57,7 +73,6 @@ export function HostnameModal({ deviceId, hostname, isOpen, closeAction }) {
           key="submit"
           onClick={() => {
             putHostname();
-            closeAction();
           }}
           icon
           positive
