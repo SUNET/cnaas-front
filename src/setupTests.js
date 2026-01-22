653d635d58757b85b6eb8ac31f8f02ed5f2f82b4
Johannes Jeppsson <johannes@sunet.se>
2026-01-22 14:37:50 +0100

    add test

--- src/setupTests.js
+++ src/setupTests.js
@@ -24,3 +24,18 @@ class LocalStorageMock {
 }
 
 globalThis.localStorage = new LocalStorageMock();
+
+// Global mock for react-router-dom useHistory
+const mockHistoryPush = jest.fn();
+jest.mock("react-router-dom/cjs/react-router-dom.min", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useHistory: () => ({ push: mockHistoryPush }),
+}));
+
+// Export mockHistoryPush for tests to access and assert
+globalThis.mockHistoryPush = mockHistoryPush;
+
+// Clear mocks between tests
+beforeEach(() => {
+  mockHistoryPush.mockClear();
+});
