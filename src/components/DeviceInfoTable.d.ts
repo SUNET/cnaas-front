2d58ea3743118981f59d2df7641e693312b5a817
Johannes Jeppsson <johannes@sunet.se>
2026-06-03 11:35:40 +0200

    DeviceInfoTable typescript

--- src/components/DeviceInfoTable.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Device } from "../types/device";
-
-export function DeviceInfoTable(props: {
-  device: Device | Record<string, unknown>;
-  model?: unknown;
-  netboxDevice?: unknown;
-}): JSX.Element;
