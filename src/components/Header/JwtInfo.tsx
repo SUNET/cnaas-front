eaf1a7c3b1eee32fc61f90f1b0e1d81f3832f8dd
Johannes Jeppsson <johannes@sunet.se>
2026-09-09 15:01:16 +0200

    JwtInfo use Popover component

--- src/components/Header/JwtInfo.tsx
+++ src/components/Header/JwtInfo.tsx
@@ -1,39 +1,60 @@
-import { NavLink } from "react-router";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import { Tooltip } from "../Tooltip";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import RefreshIcon from "@mui/icons-material/Refresh";
 import SettingsIcon from "@mui/icons-material/Settings";
-import { useAuthToken } from "../../stores/AuthTokenContext";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import Popover from "@mui/material/Popover";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import { useState } from "react";
+import { NavLink } from "react-router";
 import { useSecondsUntilExpiry } from "../../hooks/useSecondsUntilExpiry";
+import { useAuthToken } from "../../stores/AuthTokenContext";
 import { secondsToText } from "../../utils/formatters";
+import { Tooltip } from "../Tooltip";
 
 export function JwtInfo() {
   const { doTokenRefresh, logout, username, token, tokenExpiry } =
     useAuthToken();
   const secondsUntilExpiry = useSecondsUntilExpiry(tokenExpiry);
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+  const open = Boolean(anchorEl);
 
   const settingsHighlighted =
     process.env.NETBOX_API_URL && !localStorage.getItem("netboxToken");
 
   return (
-    <Tooltip
-      key="profile"
-      title={
-        <>
-          <p key="userinfo">
+    <>
+      <IconButton
+        aria-label="Account"
+        aria-haspopup="true"
+        aria-expanded={open}
+        onClick={(e) => setAnchorEl(e.currentTarget)}
+      >
+        <AccountCircleIcon fontSize="large" />
+      </IconButton>
+
+      <Popover
+        open={open}
+        anchorEl={anchorEl}
+        onClose={() => setAnchorEl(null)}
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
+      >
+        <Box sx={{ padding: 2, minWidth: 280 }}>
+          <Typography variant="subtitle1" sx={{ fontWeight: 600 }}>
             {username
               ? `Logged in as ${username}`
               : "Unknown user (username attribute missing)"}
-          </p>
-          <p
-            key="exp"
-            className={
+          </Typography>
+          <Typography
+            color={
               secondsUntilExpiry !== null && secondsUntilExpiry <= 0
-                ? "tokenexpired"
-                : ""
+                ? "error"
+                : undefined
             }
           >
             {secondsUntilExpiry === null && "Token does not expire."}
@@ -43,57 +64,58 @@ export function JwtInfo() {
             {secondsUntilExpiry !== null &&
               secondsUntilExpiry > 0 &&
               `Token expires in ${secondsToText(secondsUntilExpiry)}.`}
-          </p>
-          <Tooltip
-            title="Copy JWT (to use from curl etc), take note of valid time listed above"
-            placement="bottom-end"
-          >
-            <IconButton
-              size="small"
-              onClick={() => {
-                if (token) navigator.clipboard.writeText(token);
-              }}
-            >
-              <ContentCopyIcon fontSize="small" />
-            </IconButton>
-          </Tooltip>
-          <Tooltip
-            title="Try to refresh the access token now, if it can't be refresh automatically you will be asked to log in again"
-            placement="bottom-end"
-          >
-            <IconButton size="small" onClick={doTokenRefresh}>
-              <RefreshIcon fontSize="small" />
-            </IconButton>
-          </Tooltip>
-          <Tooltip
-            title="Make changes to user settings for this browser session"
-            placement="bottom-end"
-          >
-            <NavLink
-              end
-              className={({ isActive }) => (isActive ? "active" : undefined)}
-              to="/settings"
-              key="settings"
+          </Typography>
+          <Stack direction="row" sx={{ marginTop: 1 }}>
+            <Tooltip
+              title="Copy JWT (to use from curl etc), take note of valid time listed above"
+              placement="bottom-end"
             >
               <IconButton
                 size="small"
-                color={settingsHighlighted ? "warning" : "default"}
+                onClick={() => {
+                  if (token) navigator.clipboard.writeText(token);
+                }}
               >
-                <SettingsIcon fontSize="small" />
+                <ContentCopyIcon fontSize="small" />
               </IconButton>
-            </NavLink>
-          </Tooltip>
-          <p key="logout">
-            <Button variant="contained" onClick={logout}>
-              Log out
-            </Button>
-          </p>
-        </>
-      }
-    >
-      <span>
-        <AccountCircleIcon fontSize="large" />
-      </span>
-    </Tooltip>
+            </Tooltip>
+            <Tooltip
+              title="Try to refresh the access token now, if it can't be refresh automatically you will be asked to log in again"
+              placement="bottom-end"
+            >
+              <IconButton size="small" onClick={doTokenRefresh}>
+                <RefreshIcon fontSize="small" />
+              </IconButton>
+            </Tooltip>
+            <Tooltip
+              title="Make changes to user settings for this browser session"
+              placement="bottom-end"
+            >
+              <NavLink
+                end
+                className={({ isActive }) => (isActive ? "active" : undefined)}
+                to="/settings"
+              >
+                <IconButton
+                  size="small"
+                  color={settingsHighlighted ? "warning" : "default"}
+                >
+                  <SettingsIcon fontSize="small" />
+                </IconButton>
+              </NavLink>
+            </Tooltip>
+          </Stack>
+          <Divider sx={{ marginY: 1 }} />
+          <Button
+            color="secondary"
+            variant="contained"
+            fullWidth
+            onClick={logout}
+          >
+            Log out
+          </Button>
+        </Box>
+      </Popover>
+    </>
   );
 }
